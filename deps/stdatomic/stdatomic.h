3336501f615b8149b9517fca6b277ca2bbc05a5f
Stefan Johnson <sj@scrimpycat.io>
2016-11-08 06:56:20 +1100

    Remove old dependencies

--- deps/stdatomic/stdatomic.h
+++
@@ -1,7 +0,0 @@
-#if defined(__has_include_next) && __has_include_next(<stdatomic.h>)
-#include_next <stdatomic.h>
-#elif __clang__
-#include "stdatomic_clang.h"
-#else
-#include "stdatomic_portable.h"
-#endif
