56d876b0901c41e11b3fb040c1131ff77ed95bb5
Stefan Johnson <sj@scrimpycat.io>
2018-03-31 22:21:59 +1100

    Add setup script

---
+++ mac-setup.sh
@@ -0,0 +1,10 @@
+git submodule update
+
+cd deps/CommonGameKit
+sh mac-setup.sh
+
+cd ../glfw
+rm -rf build
+mkdir build && cd build
+cmake -G Xcode ..
+sed -i -e 's/SDKROOT/MACOSX_DEPLOYMENT_TARGET = 10.10; SDKROOT/' glfw.xcodeproj/project.pbxproj
