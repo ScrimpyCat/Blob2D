e449c8da3585cf5f726266bf605df02a9485d0c2
Stefan Johnson <sj@scrimpycat.io>
2019-10-07 01:17:27 +1100

    Use quick compile flag

--- src/EngineMain.c
+++ src/EngineMain.c
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define CC_QUICK_COMPILE
 #define CC_LOG_OPTION CCLogOptionOutputAll
 
 #include "EngineMain.h"
--- src/Setup/EngineSetup.c
+++ src/Setup/EngineSetup.c
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define CC_QUICK_COMPILE
 #include "EngineSetup.h"
 #include <CommonGameKit/CommonGameKit.h>
 #include "ExpressionSetup.h"
